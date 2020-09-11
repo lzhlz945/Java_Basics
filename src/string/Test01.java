39a297c99c8e6d32f550ba221e8ebd5025f7d82d
Administrator <1746678613@qq.com>
2020-09-11 10:47:04 +0800

    创建bran1

--- src/string/Test01.java
+++ src/string/Test01.java
@@ -12,7 +12,7 @@ package string;
  */
 public class Test01 {
     public static void main(String[] args) {
-
+        int a;
         String s1="abc";
         String s2="abc";
         System.out.println(s1==s2);
