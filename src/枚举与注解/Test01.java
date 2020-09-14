19202e32d5353582ee8c4d5251b7cff42cfe52ee
Administrator <1746678613@qq.com>
2020-09-14 11:01:41 +0800

    master JDK 5.0之前创建枚举

--- src/枚举与注解/Test01.java
+++ src/枚举与注解/Test01.java
@@ -1,4 +1,42 @@
 package 枚举与注解;
 
+/**
+ * 构造器私有化
+ * 变量被final修饰
+ * 只有get()方法
+ */
 public class Test01 {
+    public static void main(String[] args) {
+        Session spring = Session.SPRING;
+        System.out.println(spring);
+    }
+}
+class Session{
+    private  final String name;
+    private  final String  desc;
+
+    private Session(String name,String desc) {
+        this.name=name;
+        this.desc = desc;
+    }
+    public static final Session SPRING=new Session("春天","春暖花开");
+    public static final Session SUMMER=new Session("春天","春暖花开");
+    public static final Session AUTUMN=new Session("春天","春暖花开");
+    public static final Session WINTER=new Session("春天","春暖花开");
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Session{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
 }
