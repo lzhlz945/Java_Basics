50860e08dabea9a092bf45e4bad6fbc58503a097
Administrator <1746678613@qq.com>
2020-09-14 15:15:11 +0800

    bran1 集合contains比较的类必须重写equals方法

--- src/集合/Test01.java
+++ src/集合/Test01.java
@@ -1,8 +1,25 @@
 package 集合;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 使用比较时必须要重写equals()方法
+ */
 public class Test01 {
+
+
     public static void main(String[] args) {
-        System.out.println(00111);
-        System.out.println(123);
+
+        Collection c1=new ArrayList();
+        ((ArrayList) c1).add(123);
+        System.out.println(c1.contains(123));
+        ((ArrayList) c1).add(new Persion("zs"));
+        ((ArrayList) c1).add(new String("tt"));
+;
+        c1.contains(new String("tt"));
+        System.out.println(c1.contains(new Persion("zs")));
+        //Persion 类没有写equals方法就是false
+
     }
 }
