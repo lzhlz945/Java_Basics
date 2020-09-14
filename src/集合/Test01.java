d90ec5c2f21f6670775f9a5d26d5d82bd1a3a49c
Administrator <1746678613@qq.com>
2020-09-14 14:07:45 +0800

    bran1 初始化master

--- src/集合/Test01.java
+++ src/集合/Test01.java
@@ -2,6 +2,7 @@ package 集合;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println(001);
+        System.out.println(00111);
+        System.out.println(123);
     }
 }
