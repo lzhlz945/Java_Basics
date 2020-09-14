025aacc0ef62b78f18899018f05e455847fc7f0d
Administrator <1746678613@qq.com>
2020-09-14 16:25:08 +0800

    bran1 集合迭代器 next()[指针下移，下移后把元素返回]、hasNext();

---
+++ src/集合/Test03.java
@@ -0,0 +1,33 @@
+package 集合;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Test03 {
+    public static void main(String[] args) {
+        Collection c1=new ArrayList();
+        ((ArrayList) c1).add(123);
+        ((ArrayList) c1).add(new Persion("zs"));
+        ((ArrayList) c1).add(new String("tt"));
+        ((ArrayList) c1).add(new String("tt"));
+        ((ArrayList) c1).add(new String("tt"));
+        Iterator iterator = c1.iterator();
+//        System.out.println(iterator.hasNext());
+//        System.out.println(iterator.hasNext());
+//        System.out.println(iterator.hasNext());
+        while (iterator.hasNext()){
+            Object next = iterator.next();
+            System.out.println(next);
+            if(next.equals("tt")){
+                iterator.remove();
+            }
+        }
+        System.out.println("******************************");
+        for (Iterator iterator1 = c1.iterator(); iterator1.hasNext(); ) {
+            Object next = iterator1.next();
+            System.out.println(next);
+        }
+
+    }
+}
