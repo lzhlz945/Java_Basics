1471736faa1fc9b69794ff9597574c0901984aa0
Administrator <1746678613@qq.com>
2020-09-16 09:02:50 +0800

    bran1 集合LinkedList 有first和last add()存入到Node中

---
+++ src/集合/Test05.java
@@ -0,0 +1,15 @@
+package 集合;
+
+import java.util.LinkedList;
+
+/**
+ * LinkedList 添加、删除块
+ * ArrayList  遍历快
+ *
+ *
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        LinkedList linkedList=new LinkedList();
+    }
+}
