7457fdb4413453ed4c60e5972728f062cffaa4bf
Administrator <1746678613@qq.com>
2020-09-17 10:11:15 +0800

    bran1 集合Map

---
+++ src/集合/Test08.java
@@ -0,0 +1,12 @@
+package 集合;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test08 {
+    public static void main(String[] args) {
+
+        Map map=new HashMap();
+        map.values();
+    }
+}
