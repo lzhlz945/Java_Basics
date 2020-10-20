83a6e6885232ae3440130af4833bb455f69f9a76
Administrator <1746678613@qq.com>
2020-10-20 15:26:49 +0800

    bran1  IO --list()文件相对路径，和listFiles()绝对 命名

--- src/io流/TestIo.java
+++ src/io流/TestIo.java
@@ -30,4 +30,27 @@ public class TestIo {
         file1.getName();
         file2.getAbsoluteFile();
     }
+
+    @Test
+    public void test02(){
+        //File 的文件路径遍历和重命名
+        File file=new File("hello.txt");
+        File file2=new File("E:\\java_Thread_Basics\\src");
+
+        String[] list = file2.list();
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("************");
+        File[] files = file2.listFiles();
+        for (File file1 : files) {
+            System.out.println(file1);
+        }
+        //重命名  file3必须存在 且 file4不存在
+        File file3=new File("E:\\Java_Basics\\src\\hello.txt");
+        File file4=new File("E:\\io\\file2.txt");
+
+        boolean b = file3.renameTo(file4);
+        System.out.println(b);
+    }
 }
