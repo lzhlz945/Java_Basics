f33bb9c55ca9e0affea7e0c5c634687fadc783c3
Administrator <1746678613@qq.com>
2020-10-21 09:58:52 +0800

    bran1  IO --FileReader和FileWriter

--- src/io流/TestIo.java
+++ src/io流/TestIo.java
@@ -3,6 +3,8 @@ package io流;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class TestIo {
@@ -10,7 +12,7 @@ public class TestIo {
      * file的三种有参构造方法
      */
     @Test
-    public void test01(){
+    public void test01() {
         //1.相对路径
         File file1 = new File("hello.txt");
 
@@ -20,10 +22,10 @@ public class TestIo {
 
         //3.
 
-        File file3 = new File("E:\\io","io1");
+        File file3 = new File("E:\\io", "io1");
         //4
 
-        File file4 = new File(file3,"file1.txt");
+        File file4 = new File(file3, "file1.txt");
         System.out.println(file1);
         System.out.println(file2);
         System.out.println(file3);
@@ -33,10 +35,10 @@ public class TestIo {
     }
 
     @Test
-    public void test02(){
+    public void test02() {
         //File 的文件路径遍历和重命名
-        File file=new File("hello.txt");
-        File file2=new File("E:\\java_Thread_Basics\\src");
+        File file = new File("hello.txt");
+        File file2 = new File("E:\\java_Thread_Basics\\src");
 
         String[] list = file2.list();
         for (String s : list) {
@@ -48,8 +50,8 @@ public class TestIo {
             System.out.println(file1);
         }
         //重命名  file3必须存在 且 file4不存在
-        File file3=new File("E:\\Java_Basics\\src\\hello.txt");
-        File file4=new File("E:\\io\\file2.txt");
+        File file3 = new File("E:\\Java_Basics\\src\\hello.txt");
+        File file4 = new File("E:\\io\\file2.txt");
 
         boolean b = file3.renameTo(file4);
         System.out.println(b);
@@ -59,31 +61,130 @@ public class TestIo {
     public void test03() throws IOException {
         //文件夹和文件的创建
         File file = new File("E:\\io\\file2.txt");
-        if(!file.exists()){
+        if (!file.exists()) {
             file.createNewFile();
             System.out.println("文件创建成功");
-        }else {
+        } else {
             System.out.println("删除文件");
             file.delete();
         }
 
 
+    }
 
-   }
     @Test
     public void test04() throws IOException {
         //mkdir()不会创建上级没有的目录，mkdirs()会创建上级不存在的目录
         File file = new File("E:\\io\\file1\\file4");
         boolean mkdir1 = file.mkdir();
-        if(mkdir1){
+        if (mkdir1) {
             System.out.println("文件1创建成功");
-        }else {
+        } else {
             System.out.println("文件1创建失败");
         }
         File file2 = new File("E:\\io\\file2\\file3");
         boolean mkdir2 = file2.mkdirs();
-        if(mkdir2){
+        if (mkdir2) {
             System.out.println("文件2创建成功");
         }
+    }
+
+    /**
+     * 一、流IO：
+     * FileReader
+     */
+    @Test
+    public void test05() {
+        String fis = "E:\\io\\file2\\hello.txt";
+        try (
+                FileReader fileReader = new FileReader(fis);
+        ) {
+            int data;
+            while ((data = fileReader.read()) != -1) {
+
+                System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test06() {
+        String fis = "E:\\io\\file2\\hello.txt";
+        try (
+                FileReader fileReader = new FileReader(fis)
+        ) {
+            char[] chars = new char[1028];
+            while ((fileReader.read(chars)) != -1) {
+
+                String str = new String(chars, 0, chars.length);
+                System.out.print(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * FileWriter
+     */
+    @Test
+    public void test07() {
+        String fis = "E:\\io\\file2\\hello1.txt";
+        try (
+                FileWriter fileReader = new FileWriter(fis)
+        ) {
+
+            fileReader.write("hello");
+            fileReader.write("_world");
+            fileReader.write("\n");
+            fileReader.write("china");
+            fileReader.write("+++++");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     /**
+      * FileReader、writer读写文件
+      *
+      */
+     @Test
+     public void test08() {
+
+           String fis="E:\\io\\file2\\hello.txt";
+           String fws="E:\\io\\file2\\hello1.txt";
+           try (FileReader fileReader = new FileReader(fis);
+                  FileWriter fileWriter = new FileWriter(fws) ){
+               System.out.println();
+               char[] chars=new char[1];
+               int len;
+               while ((len=fileReader.read(chars))!=-1){
+                   fileWriter.write(chars,0,len);
+
+               }
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+
+       }
+    @Test
+    public void test09() {
+
+        String fis="E:\\io\\file2\\hello.txt";
+        String fws="E:\\io\\file2\\hello1.txt";
+        try (FileReader fileReader = new FileReader(fis);
+             FileWriter fileWriter = new FileWriter(fws) ){
+            System.out.println();
+            char[] chars=new char[5];
+
+            while ((fileReader.read(chars))!=-1){
+                fileWriter.write(chars);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
